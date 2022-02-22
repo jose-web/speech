e950ad451eedd5b9f9fa68473b71add7c506dc18
jose <delossantosramosjose@gmail.com>
2022-02-22 23:51:26 +0100

    Basic functionality added

---
+++ script.js
@@ -0,0 +1,26 @@
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+
+const recognition = new SpeechRecognition()
+recognition.continuous = true;
+recognition.lang = 'es-ES';
+
+window.onload = () => {
+    recognition.start()
+}
+
+recognition.onresult = (event) => {
+    let current = event.resultIndex
+    let transcript = event.results[current][0].transcript
+    console.log(transcript)
+    speak(transcript)
+}
+
+function speak(message){
+    let voice = new SpeechSynthesisUtterance()
+    voice.text = message
+    voice.volume = 1
+    voice.rate = 1
+    voice.pitch = 1
+
+    window.speechSynthesis.speak(voice)
+}
